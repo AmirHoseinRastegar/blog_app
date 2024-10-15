75c7266b3c049385c2b086d3cfa4d0ab97e1efe0
amirpooyani19 <dota2ker@yahoo.com>
2024-10-15 23:30:16 +0330

    23 Mehr 1403 12:43 PM

--- lib/features/bookmark/presentation/screens/bookmark_screen.dart
+++ lib/features/bookmark/presentation/screens/bookmark_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import '../../domain/entities/bookmark_entity.dart';
 
 class BookmarksScreen extends StatefulWidget {
@@ -21,35 +22,42 @@ class _BookmarksScreenState extends State<BookmarksScreen> {
   void _loadBookmarks() {
     final box = Hive.box<BookMarkEntity>('bookMarks');
     setState(() {
-
       bookmarks = box.values.toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final box = Hive.box<BookMarkEntity>('bookMarks');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bookmarked Posts'),
       ),
-      body: bookmarks.isEmpty
-          ? const Center(child: Text('No bookmarks yet!'))
-          : ListView.builder(
-        itemCount: bookmarks.length,
-        itemBuilder: (context, index) {
-          final bookmark = bookmarks[index];
-          return ListTile(
-            title: Text(bookmark.title),
-            subtitle: Text(
-              bookmark.content.length > 50
-                  ? '${bookmark.content.substring(0, 50)}...'
-                  : bookmark.content,
-            ),
-            leading: bookmark.imageUrl != null
-                ? Image.network(bookmark.imageUrl!, width: 50)
-                : const Icon(Icons.bookmark),
-            onTap: () {
+      body: ValueListenableBuilder(
+        valueListenable: box.listenable(),
+        builder:
+            (BuildContext context, Box<BookMarkEntity> value, Widget? child) {
+          final bookmarks = box.values.toList();
 
+          if (bookmarks.isEmpty) {
+            return const Center(child: Text('No bookmarks yet!'));
+          }
+          return ListView.builder(
+            itemCount: bookmarks.length,
+            itemBuilder: (context, index) {
+              final bookmark = bookmarks[index];
+              return ListTile(
+                title: Text(bookmark.title),
+                subtitle: Text(
+                  bookmark.content.length > 50
+                      ? '${bookmark.content.substring(0, 50)}...'
+                      : bookmark.content,
+                ),
+                leading: bookmark.imageUrl != null
+                    ? Image.network(bookmark.imageUrl!, width: 50)
+                    : const Icon(Icons.bookmark),
+                onTap: () {},
+              );
             },
           );
         },
